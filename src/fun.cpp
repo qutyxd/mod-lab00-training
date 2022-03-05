edc95a8b288ce579d3bba202b52cb4907bc9dd08
Darya Nikolaenko <neeko.niiko.ni@gmail.com>
2022-03-05 12:45:40 +0300

    changed function

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,9 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    int64_t answer = x;
+    for (uint16_t i = 0; i < n - 1; i++) {
+        answer = answer * x;
+    }
+    return answer;
 }
